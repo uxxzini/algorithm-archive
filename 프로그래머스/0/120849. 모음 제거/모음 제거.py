863bf6a9a228e79b52aa86ebfa547d8917c57c3b
YuJin PARK <yjin5268@naver.com>
2026-01-27 13:44:31 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    vowels = "aeiou"
+    result = ""
+    
+    for ch in my_string:
+        if ch not in vowels:
+            result += ch
+            
+    return result
